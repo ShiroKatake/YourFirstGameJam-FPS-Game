ba0c8d08372ca4da76a4cc23428d944b24cf5a5e
ShiroKatake <tgkhang262@gmail.com>
2019-11-30 23:32:58 +1100

    Created MouseManager
    - This script allows for object selection using mouse hovering

---
+++ YFGJ_fps/Assets/FPS/Scripts/MouseManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+	public LayerMask switchableLayerMask;
+	public GameObject selectedObject;
+	public string hitObject;
+
+	private int m_layerMask;
+
+	private void Start() {
+		m_layerMask = LayerMask.GetMask(switchableLayerMask.ToString());
+	}
+
+	private void Update() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo;
+		if(Physics.Raycast(ray, out hitInfo, switchableLayerMask)) {
+			hitObject = hitInfo.collider.name;
+			if (hitInfo.collider.gameObject.tag == "Switchable") {
+				GameObject hitObject = hitInfo.transform.root.gameObject;
+				SelectObject(hitObject);
+			} else {
+				ClearSelection();
+			}
+		} else {
+			ClearSelection();
+		}
+	}
+
+	void SelectObject(GameObject obj) {
+		if(selectedObject != null) {
+			if (obj == selectedObject)
+				return;
+			ClearSelection();
+		}
+
+		selectedObject = obj;
+	}
+
+	void ClearSelection() {
+		selectedObject = null;
+	}
+}
