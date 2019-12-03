bcae270ef1646ab93b4ab80d215930c7bfca6fc9
ShiroKatake <tgkhang262@gmail.com>
2019-12-03 14:58:28 +1100

    Added a list of switchable enemies

--- YFGJ_fps/Assets/FPS/Scripts/EnemyManager.cs
+++ YFGJ_fps/Assets/FPS/Scripts/EnemyManager.cs
@@ -5,17 +5,21 @@ using UnityEngine.Events;
 public class EnemyManager : MonoBehaviour {
 
 	public List<EnemyController> enemies { get; private set; }
+	public List<EnemyController> switchables { get; private set; }
 	public int numberOfEnemiesTotal { get; private set; }
 	public int numberOfEnemiesRemaining => enemies.Count;
 
 	public UnityAction<EnemyController, int> onRemoveEnemy;
 
 	private void Awake() {
-
+		switchables = new List<EnemyController>();
 		enemies = new List<EnemyController>();
 	}
 
 	public void RegisterEnemy(EnemyController enemy) {
+		if (enemy.gameObject.tag == "Switchable") {
+			switchables.Add(enemy);
+		}
 		enemies.Add(enemy);
 
 		numberOfEnemiesTotal++;
@@ -27,6 +31,7 @@ public class EnemyManager : MonoBehaviour {
 		onRemoveEnemy.Invoke(enemyKilled, enemiesRemainingNotification);
 
 		// removes the enemy from the list, so that we can keep track of how many are left on the map
+		switchables.Remove(enemyKilled);
 		enemies.Remove(enemyKilled);
 	}
 }
